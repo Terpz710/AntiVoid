6ccc4294c5b6b6ad91c0626d53c0e71883727e63
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-02-14 08:23:53 -0800

    Updates

--- src/Terpz710/EventListener/AntiVoidEventListener.php
+++ src/Terpz710/EventListener/AntiVoidEventListener.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Terpz710\EventListener;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerMoveEvent
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
+
 use Terpz710\AntiVoid;
 
 class AntiVoidEventListener implements Listener {
@@ -33,40 +33,15 @@ class AntiVoidEventListener implements Listener {
         $y = $event->getTo()->getY();
 
         if ($y < 0) {
-            // Teleport the player back to the world spawn
             $player->teleport($player->getWorld()->getSpawnLocation());
-
-            // Get title and subtitle from config
-            $title = $this->plugin->getConfig()->get("title", "You got saved!");
-            $subtitle = $this->plugin->getConfig()->get("subtitle", "You have been teleported back to the spawn.");
-
-            // Send title and subtitle messages if enabled
+            $title = $this->plugin->getConfig()->get("title");
+            $subtitle = $this->plugin->getConfig()->get("subtitle"");
             if ($this->showTitles) {
                 $player->sendTitle($title);
             }
-
             if ($this->showSubtitles) {
                 $player->sendSubtitle($subtitle);
             }
-        }
-    }
-
-    public function onPlayerRespawn(PlayerRespawnEvent $event) : void {
-        $player = $event->getPlayer();
-        // Teleport the player back to the world spawn when they respawn
-        $player->teleport($player->getWorld()->getSpawnLocation());
-
-        // Get title and subtitle from config
-        $title = $this->plugin->getConfig()->get("title", "You got saved!");
-        $subtitle = $this->plugin->getConfig()->get("subtitle", "You have been teleported back to the spawn.");
-
-        // Send title and subtitle messages if enabled
-        if ($this->showTitles) {
-            $player->sendTitle($title);
-        }
-
-        if ($this->showSubtitles) {
-            $player->sendSubtitle($subtitle);
         }
     }
 }
